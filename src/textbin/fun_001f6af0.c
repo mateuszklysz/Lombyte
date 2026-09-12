71d5376ea919c623e41d76aecd20eed2737ecbb2
Mateusz Kłysz <mateuszklysz@icloud.com>
2026-09-12 20:03:10 +0200

    decomp: promote FUN_001f6af0/FUN_001f6b88/FUN_001f6c20 (run 9; void-callee arg order + canonical aliases)

---
+++ src/textbin/fun_001f6af0.c
@@ -0,0 +1,23 @@
+/*
+STATE: C_EXACT
+SYMBOL: FUN_001f6af0
+SCORE: code=100 functions=100 data=100 complete_data=100
+COMPILER: sn-O2
+DECISION: promoted
+BLOCKER: none
+*/
+
+#include "types.h"
+extern u8 D_001DF050[];
+extern s32 func_001F44B8();
+extern s32 func_001F6250();
+extern void func_001F62B0();
+s32 FUN_001f6af0(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4) {
+    s32 temp_16_21;
+
+    temp_16_21 = arg0 - (func_001F6250(arg3, arg4) >> 1);
+    func_001F62B0(temp_16_21, arg1, arg2, arg3, arg4, func_001F44B8(1), D_001DF050);
+    return temp_16_21;
+}
+
+extern __typeof__(FUN_001f6af0) func_001F6AF0 __attribute__((alias("FUN_001f6af0")));
---
+++ src/textbin/fun_001f6c20.c
@@ -0,0 +1,23 @@
+/*
+STATE: C_EXACT
+SYMBOL: FUN_001f6c20
+SCORE: code=100 functions=100 data=100 complete_data=100
+COMPILER: sn-O2
+DECISION: promoted
+BLOCKER: none
+*/
+
+#include "types.h"
+extern u8 D_001DF790[];
+extern s32 func_001F44B8();
+extern s32 func_001F6290();
+extern void func_001F62B0();
+s32 FUN_001f6c20(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4) {
+    s32 temp_16_21;
+
+    temp_16_21 = arg0 - (func_001F6290(arg3, arg4) >> 1);
+    func_001F62B0(temp_16_21, arg1, arg2, arg3, arg4, func_001F44B8(3), D_001DF790);
+    return temp_16_21;
+}
+
+extern __typeof__(FUN_001f6c20) func_001F6C20 __attribute__((alias("FUN_001f6c20")));
