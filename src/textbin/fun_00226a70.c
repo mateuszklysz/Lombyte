eaaf9082740bb4fb34e6aa8d9699bc66db8e01a5
Mateusz Kłysz <mateuszklysz@icloud.com>
2026-09-15 09:39:46 +0200

    decomp: promote fun_00226a70 (C_EXACT 534 / 75,252 B)

---
+++ src/textbin/fun_00226a70.c
@@ -0,0 +1,53 @@
+/*
+STATE: C_EXACT
+SYMBOL: FUN_00226a70
+SCORE: code=100 functions=100 data=100 complete_data=100
+COMPILER: sn-O2 -O2 -g2 -gstabs
+DECISION: promoted
+NOTE: input-consumer empty-asm barrier holds the LO_SUM before the call (retail addiu-before-jal + nop slot)
+*/
+
+#include "types.h"
+struct M2c_D_0013D290 {
+    u8 pad_0[0x14];
+    s32 unk14;
+    u8 pad_18[0xA8];
+    s32 unkC0;
+    u8 pad_C4[0x18];
+    s32 unkDC;
+    s32 unkE0;
+    u8 pad_E4[0x8];
+    s32 unkEC;
+    u8 pad_F0[0x4];
+    s32 unkF4;
+};
+
+extern struct M2c_D_0013D290 D_0013D290;
+extern u8 D_0015EE98[];
+extern s32 func_00209370();
+extern s32 func_0020ABB0();
+extern s32 sceCdReadClock();
+extern s32 sceScfGetLocalTimefromRTC();
+void FUN_00226a70(s32 arg0, s32 arg1) {
+    func_00209370();
+    {
+        u8 *p1 = D_0015EE98;
+        __asm__ volatile ("" : : "r" (p1));
+        sceCdReadClock(p1);
+    }
+    {
+        u8 *p1 = D_0015EE98;
+        __asm__ volatile ("" : : "r" (p1));
+        sceScfGetLocalTimefromRTC(p1);
+    }
+    func_0020ABB0(arg0);
+    D_0013D290.unkC0 = 0;
+    D_0013D290.unk14 = arg1;
+    *(s32 *)((u8 *)&D_0013D290 + (arg1 * 0x1C) + 0x20) = 0;
+    D_0013D290.unkF4 = 1;
+    D_0013D290.unkEC = arg0;
+    if (D_0013D290.unkDC < 0) {
+        D_0013D290.unkE0 = 0;
+        D_0013D290.unkDC = 0x13;
+    }
+}
