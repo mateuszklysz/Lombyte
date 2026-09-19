c2077f7fec9c44986756d8b11f36ce9d2c9e2c66
Mateusz Kłysz <mateuszklysz@icloud.com>
2026-09-19 09:55:18 +0200

    decomp: promote _decodeOrSkipField (348 B)

---
+++ src/sdk/library/_decodeOrSkipField.c
@@ -0,0 +1,127 @@
+/*
+STATE: C_EXACT
+SYMBOL: _decodeOrSkipField
+SCORE: code=100 functions=100 data=100 complete_data=100
+DECISION: promoted
+*/
+
+#include "types.h"
+
+typedef volatile s8 vs8;
+typedef volatile u8 vu8;
+typedef volatile s16 vs16;
+typedef volatile u16 vu16;
+typedef volatile s32 vs32;
+typedef volatile u32 vu32;
+typedef volatile s64 vs64;
+typedef volatile u64 vu64;
+typedef s32 b32;
+struct M2c_temp_16_13
+{
+  s32 unk0;
+  s32 unk4;
+  s32 unk8;
+  u8 pad_C[0xA0];
+  s32 unkAC;
+  u8 pad_B0[0x24];
+  s32 unkD4;
+  u8 pad_D8[0x40];
+  s32 unk118;
+  s32 unk11C;
+  s32 unk120;
+  u8 pad_124[0x50];
+  s32 unk174;
+};
+struct M2c_arg0
+{
+  u8 pad_0[0x8];
+  s32 unk8;
+  u8 pad_C[0x34];
+  struct M2c_temp_16_13 *unk40;
+};
+extern s32 _decPicture();
+extern s32 _dispatchMpegCbNodata();
+extern s32 _nextHeader();
+extern s32 _sceMpegFlush();
+extern s32 _updateRefImage();
+extern void _outputFrame();
+s32 _decodeOrSkipField(struct M2c_arg0 *arg0, s32 arg1, s32 arg2)
+{
+  struct M2c_temp_16_13 *p;
+  s32 bVar3;
+  unsigned int new_var2;
+  s32 iVar4;
+  long lVar5;
+  s32 uVar2;
+  s32 uVar7;
+  short new_var;
+  s32 gate;
+  bVar3 = 0;
+  p = arg0->unk40;
+  p->unk120 = 0;
+  if ((arg2 == (-1)) || (arg1 < arg2))
+  {
+    bVar3 = 1;
+  }
+  ;
+  if (p->unk8 == 0)
+  {
+    arg0->unk8 = 0;
+    p->unk8 = 1;
+  }
+  lVar5 = _updateRefImage(p, 0);
+  if ((lVar5 != 0) && (bVar3 != 0))
+  {
+    _decPicture(p);
+  }
+  p->unk120 = 1;
+  lVar5 = _nextHeader(p);
+  if (lVar5 == 0)
+  {
+    _sceMpegFlush(arg0);
+    p->unk0 = 1;
+    return 0;
+  }
+  iVar4 = 2;
+  if (p->unkD4 != 1)
+  {
+    iVar4 = 1;
+  }
+  if (p->unk174 != iVar4)
+  {
+    return -1;
+  }
+  new_var2 = _updateRefImage(p, 1);
+  gate = 0;
+  if (new_var2 != 0)
+  {
+    gate = 1;
+  }
+  uVar7 = 0;
+  if (gate != 0)
+  {
+    if (bVar3 == 0)
+    {
+      uVar2 = p->unk118;
+      goto out;
+    }
+    lVar5 = _decPicture(p);
+    if (lVar5 != 0)
+    {
+      uVar7 = 1;
+    }
+  }
+  ;
+  out:
+  _outputFrame(p, p->unk118, p->unk4);
+
+  p->unk120 = 0;
+  arg0->unk8 = p->unk118 - p->unkAC;
+  p->unk118 = p->unk118 + 1;
+  p->unk4 = (unsigned long long) (p->unk4 + 1);
+  if (bVar3 == 0)
+  {
+    _dispatchMpegCbNodata(arg0);
+  }
+  return uVar7;
+}
---
+++ src/sdk/sif_rpc/sce_sif_load_module_buffer.c
@@ -0,0 +1,49 @@
+/*
+STATE: C_EXACT
+SYMBOL: _sceSifLoadModuleBuffer
+SCORE: code=100 functions=100 data=100 complete_data=100
+DECISION: promoted
+*/
+
+#include "types.h"
+struct M2c_D_00158200 {
+    s32 unk0;
+    s32 unk4;
+};
+extern u8 D_00158200[];
+extern u8 D_00158400[];
+extern s32 _lf_bind();
+extern s32 func_0011CAE0();
+extern s32 memcpy();
+extern s32 sceSifCallRpc();
+#define SIFCMD ((struct M2c_D_00158200 *) D_00158200)
+s32 _sceSifLoadModuleBuffer(s32 arg0, s32 arg1, s32 arg2, s32 *arg3) {
+    s32 var_2_18;
+    s32 temp;
+
+    var_2_18 = 0xFFFF0000;
+    if (_lf_bind() < 0) {
+        return var_2_18;
+    }
+    if (func_0011CAE0() != 0) {
+        return 0xFFFEFFFC;
+    }
+    SIFCMD->unk0 = arg0;
+    if (arg2 != 0) {
+        if (arg1 >= 0xFD) {
+            memcpy(D_00158200 + 0x104, (void *) arg2, 0xFC);
+            SIFCMD->unk4 = 0xFC;
+        } else {
+            memcpy(D_00158200 + 0x104, (void *) arg2, arg1);
+            SIFCMD->unk4 = arg1;
+        }
+    } else {
+        SIFCMD->unk4 = 0;
+    }
+    if (sceSifCallRpc(D_00158400, 6, 0, D_00158200, 0x200, D_00158200, 8, 0, 0) < 0) {
+        return 0xFFFEFFFF;
+    }
+    temp = SIFCMD->unk0;
+    *arg3 = SIFCMD->unk4;
+    return temp;
+}
